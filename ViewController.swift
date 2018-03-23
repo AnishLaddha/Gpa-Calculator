f69b5f26086c9351f04593b46fa880dc347da6cb
Anish Laddha <anishladdha03@gmail.com>
2018-03-22 17:32:27 -0700

    Initial commit

---
+++ ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  Gpa Calculator
+//
+//  Created by Anish Laddha on 2/26/18.
+//  Copyright © 2018 Anish Laddha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var UsrGrade: UITextField!
+    @IBOutlet weak var H1: UISwitch!
+    @IBOutlet weak var H2: UISwitch!
+    @IBOutlet weak var H3: UISwitch!
+    @IBOutlet weak var H4: UISwitch!
+    @IBOutlet weak var H5: UISwitch!
+    @IBOutlet weak var H6: UISwitch!
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
